11c6606ea1e7a4aea4857ad5bff47b92d39a553d
neeraj0003 <ineeraj0003@gmail.com>
2025-01-27 00:48:42 +0530

    Add Project Files

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,5 +1,6 @@
+const Listing = require("../models/listing.js"); 
 const Review = require("../models/review.js");
-const Listing = require("../models/listing.js") 
+
 
 module.exports.createReview = async(req,res) => {
     let listing = await Listing.findById(req.params.id);
