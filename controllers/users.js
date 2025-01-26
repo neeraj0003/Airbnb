a8854f0190320dce20beee7d960f5fd7a0f45738
neeraj0003 <ineeraj0003@gmail.com>
2025-01-27 02:07:46 +0530

    n

--- controllers/users.js
+++ controllers/users.js
@@ -31,7 +31,7 @@ module.exports.renderLogInForm = (req, res) => {
 //actual login is done by passport but for simplicity we are giving this name
 //this callback function active after successful login
 module.exports.login = async (req, res) => {
-    req.flash("success", `Welcome to Aasaan Aavaas! ${req.body.username}`);
+    req.flash("success", `Welcome to wandelust! ${req.body.username}`);
     //if we directly click login 
     let redirectUrl = res.locals.redirectUrl || "/listings";
     res.redirect(redirectUrl);
