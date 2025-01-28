34b534b21a8d49e8ee62a8376c094cc040197210
neeraj0003 <ineeraj0003@gmail.com>
2025-01-29 01:35:48 +0530

    m

--- routes/listing.js
+++ routes/listing.js
@@ -7,7 +7,7 @@ const listingController = require("../controllers/listings.js");
 const multer  = require("multer");
 const {storage} = require("../cloudConfig.js");
 const upload = multer({ storage });
-
+const cloudConfig = require('../cloudConfig.js');
 
 router
 .route("/")
