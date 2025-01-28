869d0e94904231bc6d0042c80fd5a44b0bd80c0d
neeraj0003 <ineeraj0003@gmail.com>
2025-01-29 02:26:41 +0530

    m

--- models/listing.js
+++ models/listing.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review.js");
-const cloudConfig = require('../cloudConfig.js'); // Ensure this path is correct
 
 const listingSchema = new Schema({
     title: {
