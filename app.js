cef035995dcfe77cca1528d791f4badd3fe1d40f
neeraj0003 <ineeraj0003@gmail.com>
2025-01-31 00:28:24 +0530

    m

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
+
+// console.log(process.env.SECRET);
 
 const express = require("express");
 const app = express();
@@ -8,53 +10,57 @@ const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const ExpressError = require("./utils/ExpressError");
+const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+
 const MongoStore = require("connect-mongo");
+
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const User = require("./models/user");
-const listingRouter = require("./routes/listing");
-const reviewRouter = require("./routes/review");
-const userRouter = require("./routes/user");
+const User = require("./models/user.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
-const dbUrl = process.env.ATLASDB_URL || "mongodb://localhost:27017/yourLocalDB";
-
-mongoose.connect(dbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+// const dbUrl = process.env.ATLASDB_URL;
+const dbUrl =
+  process.env.ATLASDB_URL || "mongodb://localhost:27017/yourLocalDB";
+main()
   .then(() => {
-    console.log("Connected to DB");
+    console.log("connected to DB");
   })
   .catch((err) => {
-    console.error("DB Connection Error:", err);
+    console.log(err);
   });
+
+async function main() {
+  // await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
+}
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.engine("ejs", ejsMate);
-
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   crypto: {
-    secret: process.env.SECRET || 'thisshouldbeabettersecret',
+    secret: process.env.SECRET,
   },
   touchAfter: 24 * 3600,
 });
 
-store.on("error", (err) => {
-  console.error("ERROR in MONGO SESSION STORE", err);
+store.on("error", () => {
+  console.log("ERROR in MONGO SESSION STORE", err);
 });
 
 const sessionOptions = {
   store,
-  secret: process.env.SECRET || 'thisshouldbeabettersecret',
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -63,6 +69,10 @@ const sessionOptions = {
     httpOnly: true,
   },
 };
+
+// app.get("/", (req, res) =>  {
+//     res.send("Hi, I am root");
+// });
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -81,6 +91,7 @@ app.use((req, res, next) => {
   next();
 });
 
+// app.use("/listings",listings);
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
@@ -90,10 +101,11 @@ app.all("*", (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message = "Something went wrong!" } = err;
-  res.status(statusCode).render("error.ejs", { statusCode, message });
+  let { statusCode = 500, message = "Something went wrong!" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+  // res.status(statusCode).send(message);
 });
 
 app.listen(8080, () => {
-  console.log("Server is listening on port 8080");
+  console.log("server is listening to port 8080");
 });
